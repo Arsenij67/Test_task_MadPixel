09a1531aa706ec8cf6db5815054171924b4db42f
Arsenij <96657896+Arsenij67@users.noreply.github.com>
2025-01-29 16:25:08 +0300

    Исправлена прерывающаяся музыка при перезагрузке

--- Assets/Scripts/Audio/AudioSwitcher.cs
+++ Assets/Scripts/Audio/AudioSwitcher.cs
@@ -1,10 +1,41 @@
 using UnityEngine;
-using System;
-namespace Game.Audio {
-    public class AudioSwitcher :MonoBehaviour {
-        
-        public void SwitchVolume(bool value) {
-            AudioListener.volume = Convert.ToInt32(value);
+namespace Game.Audio
+{
+    public class AudioSwitcher : MonoBehaviour
+    {
+        private static AudioSwitcher _instance;
+        public static AudioSwitcher Instance
+        {
+            get
+            {
+                // Если экземпляр не существует, создаем новый объект
+                if (_instance == null)
+                {
+                    GameObject singletonObject = new GameObject(typeof(AudioSwitcher).Name);
+                    _instance = singletonObject.AddComponent<AudioSwitcher>();
+                    DontDestroyOnLoad(singletonObject);
+                }
+                return _instance;
+            }
+        }
+
+        private void Awake()
+        {
+            // Если уже существует экземпляр, уничтожаем этот объект
+            if (_instance != null && _instance != this)
+            {
+                Destroy(this.gameObject);
+            }
+            else
+            {
+                _instance = this;
+                DontDestroyOnLoad(this.gameObject);
+            }
+        }
+
+        public void SwitchVolume(bool value)
+        {
+            AudioListener.volume = value ? 1 : 0; // Устанавливаем громкость в 0 или 1 на основе значения
         }
     }
 }
